733d8ecba1f607a7978d415ef9e02344dd4d096f
Claire <clairelogan16@gmail.com>
2021-08-12 14:53:28 -0700

    commit tree f0def65987007b090883fe13ee64247b190e8fb7

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,8 @@
-
 class Person
-  def initialize(attributes)
-    attributes.each do |key, value|
-      self.class.attr_accessor(key)
-      self.send("#{key}=", value)
+    def initialize(attributes)
+      attributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
     end
   end
-end
---
+++ lib/personOG.rb
@@ -0,0 +1,9 @@
+
+class Person
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+end
